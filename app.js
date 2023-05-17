12bfb99a59fa4ee887b7b6601bf16fa6c6431245
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-05-17 23:52:34 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require("express-handlebars")
+
+const app = express()
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const app = express()
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
@@ -21,9 +24,11 @@ db.once('open', () => {
 })
 
 
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs"}))
+app.set("view engine", "hbs")
 
-app.get('/',(req, res) =>{
-  res.send('hello world')
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen(3000, () => {
