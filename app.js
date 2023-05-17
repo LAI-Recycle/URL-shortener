8330b72f3d62100a3bcffe7a7f81a2827f2b029a
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-05-17 23:44:25 +0800

    feat: add mongodb

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 
 app.get('/',(req, res) =>{
@@ -9,3 +29,5 @@ app.get('/',(req, res) =>{
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
+
+
