d7ee8848c7c3064490e7a2e7f246efe30aab74e2
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-05-22 23:15:55 +0800

    feat: add schema

---
+++ models/shorturl.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const shorturlSchema = new Schema({
+  originalUrl: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+ shortenedUrl: {
+    type: String
+  }
+})
+module.exports = mongoose.model('ShortUrl', shorturlSchema)
