e388b11477501741f2f3d3d5f9322ac8589b9839
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-06-05 22:35:12 +0800

    refactor: modify mongoose config

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
